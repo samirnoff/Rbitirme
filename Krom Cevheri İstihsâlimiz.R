a59ea8a7e82093e36931312b247914e0213bb380
samirnoff <aldag@itu.edu.tr>
2019-09-18 20:30:51 +0300

    İlk veri seti ve grafik

---
+++ Krom Cevheri İstihsâlimiz.R
@@ -0,0 +1,8 @@
+ggplot(KromCevheri, aes(tarih,miktar)) + geom_line(size=3) + 
+  theme(axis.title = element_blank()) +
+  labs(title="KROM CEVHERÝ ÝSTÝHSÂLÝMÝZ",
+       subtitle="TON",
+       caption="Üç sýfýr ilavesile") +
+  theme(plot.title = element_text(hjust = 0.5, face="italic")) +
+  theme(plot.caption = element_text(hjust=0, face="italic"))
+
