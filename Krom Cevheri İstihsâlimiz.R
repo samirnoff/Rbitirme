0609182fd9706234d09d29f5897a59a571891a42
DogukanTopcuoglu <55716313+DogukanTopcuoglu@users.noreply.github.com>
2019-09-23 23:15:31 +0300

    Delete Krom Cevheri İstihsâlimiz.R

--- Krom Cevheri İstihsâlimiz.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2);
-ggplot(KromCevheri, aes(tarih,miktar)) + geom_line(size=3) + 
-  theme(axis.title = element_blank()) +
-  labs(title="KROM CEVHERÝ ÝSTÝHSÂLÝMÝZ",
-       subtitle="TON",
-       caption="Üç sýfýr ilavesile") +
-  theme(plot.title = element_text(hjust = 0.5, face="italic")) +
-  theme(plot.caption = element_text(hjust=0, face="italic")) +
-  theme(axis.text.x = element_text(angle = 90))
