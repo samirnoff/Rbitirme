c20003b3b92a2467bcbd0538bbe05da73a4db2ab
DogukanTopcuoglu <55716313+DogukanTopcuoglu@users.noreply.github.com>
2019-09-23 23:14:36 +0300

    Add files via upload

---
+++ Krom Cevheri.R
@@ -0,0 +1,11 @@
+library(ggplot2);
+ggplot(KromCevheri, aes(tarih,miktar)) + geom_line(size=3) + 
+    labs(title="KROM CEVHERÝ ÝSTÝHSÂLÝMÝZ",
+       subtitle="TON",
+       caption="Üç sýfýr ilavesile") +
+  theme_bw() +
+  theme(axis.title = element_blank()) +
+  theme(plot.title = element_text(hjust = 0.5, face="italic")) +
+  theme(plot.caption = element_text(hjust=0, face="italic")) +
+  theme(axis.text.x = element_text(angle = 90))
+ 
