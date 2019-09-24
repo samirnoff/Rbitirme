868a3624c9f733b129775e1589aab43f5eefd38f
samirnoff <54136421+samirnoff@users.noreply.github.com>
2019-09-24 20:14:43 +0300

    DataSet tanımlama ve scale_y_cont.
    Dataset artık ayrı dosyadan okunmayıp direkt script çalıştırıldığında tanımlanıyor.

--- Krom Cevheri.R
+++ Krom Cevheri.R
@@ -1,12 +1,22 @@
-library(ggplot2);
+tarih <- c(1923,1924,1925,1926,1927,1928,1929,1930,1931,1932,1933,1934,1935,1936,1937)
+miktar <- c(0,4,7,4,18,10,15,25,22,50,70,115,145,160,190)
+KromCevheri <- data.frame(tarih,miktar)
 ggplot(KromCevheri, aes(tarih,miktar)) + geom_line(size=3) + 
-  labs(caption="Üç sıfır ilavesile") +
+  labs(caption=
+         "Üç sıfır 
+ilavesile") +
   theme_bw() +
   theme(axis.title = element_blank()) +
-  theme(axis.text.x = element_text(angle = 90, face = "bold")) +
-  theme(plot.caption = element_text(hjust=0, face="bold")) +
+  theme(axis.text.x = element_text(angle = 90, face = "bold", size = 15)) +
+  theme(axis.text.y = element_text(face="bold", size=14)) +
+  theme(plot.caption = element_text(hjust=0, face="bold", size=13)) +
   scale_x_continuous(
     limits = c(1923,1937),
     breaks = c(1923,1924,1925,1926,1927,1928,1929,1930,1931,1932,1933,1934,1935,1936,1937),
     label = c("1923","24","25","26","27","28","29","1930","31","32","33","34","35","36","1937")
-    )
+  ) +
+  scale_y_continuous(
+    limits = c(0,220),
+    breaks = c(0,20,40,60,80,100,120,140,160,180,200,220),
+    label = c("0","20","40","60","80","100","120","140","160","180","200","TON")
+  )
