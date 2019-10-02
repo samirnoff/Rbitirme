dbb094dfaa67c777e1916ca17aa350f2b5f963bd
samirnoff <54136421+samirnoff@users.noreply.github.com>
2019-10-02 22:39:28 +0300

    Update SarapSatisi.R

--- SarapSatisi.R
+++ SarapSatisi.R
@@ -2,18 +2,20 @@ library(ggplot2)
 tarih <- c(1932,1933,1934,1935,1936,1937)
 miktar <- c(52000,230000,200200,230000,715000,900000)
 sarapsatisi <- data.frame(tarih,miktar)
-ggplot(sarapsatisi, aes(tarih,miktar)) + geom_line(size=1.2) +
-  labs(title = "Þarap satýþý",
-       subtitle = "KÝLO") +
-  theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5, size = 17, face = "italic")) +
+ggplot(sarapsatisi, aes(tarih,miktar)) + geom_line(size=1.5) +
+  labs(title = "Åžarap satÄ±ÅŸÄ±",
+       subtitle = "KÄ°LO") +
+  theme_classic() +
+  theme(panel.grid.major = element_line(colour = "black", size = 1)) +
+  theme(plot.title = element_text(hjust = 0.5, size = 17, face = "bold.italic")) +
   theme(axis.title = element_blank()) +
-  theme(axis.text.x = element_text(size=15, face="bold")) +
+  theme(axis.text.x = element_text(size=15, face="bold", vjust = 0.5)) +
   theme(axis.text.y = element_text(size=16, face="bold")) +
+  theme(axis.line = element_line(size=2)) +
   scale_x_continuous(
     limits = c(1932,1937.5),
-    breaks = c(1932,1933,1934,1935,1936,1937),
-    labels = c("1932","3","4","5","6","7")
+    breaks = seq(1932,1938,by = 0.33),
+    labels = c("1932","","","3","","","4","","","5","","","6","","","7","","","")
   ) +
   scale_y_continuous(
     limits = c(50000,1000000),
