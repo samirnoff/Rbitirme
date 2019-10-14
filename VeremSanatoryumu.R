4439a9ce19fc878abfdd142cf66b57c3dda39d74
DogukanTopcuoglu <55716313+DogukanTopcuoglu@users.noreply.github.com>
2019-10-15 00:20:22 +0300

    Add files via upload

---
+++ VeremSanatoryumu.R
@@ -0,0 +1,38 @@
+library(ggplot2)
+tarih <- seq(1924,1937)
+miktar <- c(20,120,110,90,89,120,125,133,130,207,240,265,290,290)
+miktar2 <- c(NA,310,400,390,392,395,420,460,455,550,565,580,640,660)
+verem <- data.frame(tarih,miktar,miktar2)
+ggplot(verem, aes(x=tarih)) +
+  geom_line(aes(y=miktar), size=1.3) + geom_point(aes(y=miktar), size=3)+
+  geom_line(aes(y=miktar2), size=1.3, linetype="dashed") + geom_point(aes(y=miktar2), size=3) +
+  labs(title = "HEYBELÝADA VEREM SANATORYUMU MESAÝSÝ
+  1924 \U2013 1937",
+       subtitle = "Yarým Lugaritmik Diyaðram") +
+  theme_classic() +
+  theme(panel.grid.major = element_line(colour = "black", size = 1)) +
+  theme(plot.title = element_text(hjust = 0.5, size = 17, face = "bold")) +
+  theme(plot.subtitle = element_text(hjust = 0.5, size = 17, face = "bold.italic")) +
+  theme(axis.title = element_blank())  +
+  theme(axis.text.x = element_text(size=15, face="bold", vjust = 0.7, hjust = 1.2, angle = 90, colour = "black")) +
+  theme(axis.text.y = element_text(size=12, face="bold", colour = "black")) +
+  theme(axis.line.x = element_line(size = 1.4)) +
+  theme(axis.line.y = element_line(size = 1.4)) +
+  
+  scale_x_continuous(
+    limits = c(1924,1937),
+    breaks = seq(1924,1937),
+    labels = c("1924","1925","1926","1927","1928","1929","1930","1931","1932","1933","1934","1935","1936","1937"),
+    expand = c(0,0.5)
+  ) +
+  scale_y_continuous(
+    limits = c(0,670),
+    breaks = c(0,50,100,140,180,220,250,280,300,320,340,370,470,570,670),
+    labels = c("0","50","100","2","3","4","500","6","7","8","9","1.000","5.000","10.000","14.000"),
+    expand = c(0,0)
+  ) +
+  annotate(geom="rect", xmin = 1925.1, xmax = 1930.9, ymin = 540, ymax = 620, fill = "white" ) +
+  annotate(geom="text", x=1928.2,y=590,label="Yatýrýlarak tedavi", size= 4.7, fontface="bold.italic") +
+  annotate(geom="rect", xmin = 1925.5, xmax = 1926.5, ymin = 588, ymax = 590, fill = "black" ) +
+  annotate(geom="text", x=1928.8,y=560,label="Laboratuvar ve Rontken ", size= 4.7, fontface="bold.italic") +
+  annotate(geom="text", x=1926.1,y=560,label="- - - -  ", size= 6, fontface="bold")
