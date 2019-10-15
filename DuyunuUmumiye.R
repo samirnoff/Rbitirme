aba34e7706b8ae6c84ec9e1c9422b2dcdc90303e
gulinan <56047198+gulinan@users.noreply.github.com>
2019-10-15 19:37:21 +0300

    Update DuyunuUmumiye.R

--- DuyunuUmumiye.R
+++ DuyunuUmumiye.R
@@ -1,3 +1,4 @@
+Sys.setlocale(locale = "Turkish")
 library(ggplot2)
 tarih <- seq(1902,1925)
 miktar <- c(2300000,2300500,2200000,2400000,2500000,2600000,2500000,2700000,3000000,NA,NA,NA,NA,NA,5200000,5300000,5700000,5200000,5200000,5100000,5000000,4700000,5500000,6100000)
@@ -7,8 +8,8 @@ duyunu <- data.frame(tarih,miktar,tarih2,miktar2)
 ggplot(duyunu, aes(tarih)) +
   geom_line(aes(y=miktar), size=1.3) + geom_point(aes(y=miktar), size=2) +
   geom_line(aes(x=tarih2, y=miktar2), linetype="dashed",size=1.3)+
-  labs(title = "Duyunu Umumiye ve Devlet Ýnhisarý
-         Zamanlarýndaki safi içki varidatý") +
+  labs(title = "Duyunu Umumiye ve Devlet Ä°nhisarÄ±
+         ZamanlarÄ±ndaki safi iÃ§ki varidatÄ±") +
   theme_classic() +
   theme(panel.grid.major = element_line(colour = "black", size = 1)) +
   theme(plot.title = element_text(hjust = 0.5, size = 25, face = "bold.italic")) +
@@ -33,6 +34,6 @@ ggplot(duyunu, aes(tarih)) +
                "lira cari    "," "," "," "),
     expand = c(0,0)
     ) +
-  annotate(geom="text", x=1913.5,y=6900000,label= " \"1 altýn lira   10,60 carilira\" ", size= 5, fontface="bold.italic") +
+  annotate(geom="text", x=1913.5,y=6900000,label= " \"1 altÃ½n lira   10,60 carilira\" ", size= 5, fontface="bold.italic") +
   annotate(geom="rect", xmin = 1908.5, xmax = 1918.5, ymin = 6740000, ymax = 6770000, fill = "black" ) +
   annotate(geom="rect", xmin = 1908.5, xmax = 1918.5, ymin = 6700000, ymax = 6730000, fill = "black" ) 
