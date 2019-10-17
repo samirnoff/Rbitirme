7aabae953430b687d446face79a6d468fc101bf6
samirnoff <54136421+samirnoff@users.noreply.github.com>
2019-10-17 15:46:50 +0300

    added 2404

--- TalebeSayisi.R
+++ TalebeSayisi.R
@@ -2,8 +2,11 @@ Sys.setlocale(locale = "Turkish")
 library(ggplot2)
 tarih <- seq(1923,1937)
 miktar <- c(2528,3771,4138,4558,5022,5749,5410,5536,5298,2059,2726,2477,2805,2699,2404)
-talebe <- data.frame(tarih,miktar)
-ggplot(talebe, aes(tarih,miktar)) + geom_line(size=2) +
+tarih2 <- c(1937)
+miktar2 <- c(2404)
+talebe <- data.frame(tarih,miktar,tarih2,miktar2)
+ggplot(talebe, aes(tarih,miktar)) + geom_line(size=2) + 
+  geom_text(label = miktar2, hjust = -0.1, vjust = 0.1  , aes(tarih2,miktar2)) +
   theme_classic() +
   theme(panel.grid.major = element_line(colour = "black", size = 0.75)) +
   labs(
@@ -16,6 +19,8 @@ ggplot(talebe, aes(tarih,miktar)) + geom_line(size=2) +
   theme(axis.text.x = element_text(angle = 65, face = "bold", vjust = 1, hjust = 1.15, size = 10, colour = "black")) +
   theme(axis.text.y = element_text(size = 14, face = "bold", colour = "black")) +
   theme(axis.line = element_line(size = 1)) + 
+  theme(plot.margin = unit(c(2,3,3,3), "lines")) +
+  coord_cartesian(clip = 'off') +
   scale_x_continuous(
     limits = c(1923,1937),
     breaks = seq(1923,1937),
@@ -43,5 +48,5 @@ ggplot(talebe, aes(tarih,miktar)) + geom_line(size=2) +
   annotate(geom = "text", x = 1933.5, y = 2726, label = "2726", fontface = "bold") +
   annotate(geom = "text", x = 1934.5, y = 2477, label = "2277", fontface = "bold") +
   annotate(geom = "text", x = 1935.5, y = 2900, label = "2805", fontface = "bold") +
-  annotate(geom = "text", x = 1936.5, y = 2750, label = "2699", fontface = "bold") +
-  annotate(geom = "text", x = 1936.6, y = 2352, label = "2404", fontface = "bold")
+  annotate(geom = "text", x = 1936.5, y = 2750, label = "2699", fontface = "bold")
+#  annotate(geom = "text", x = 1936.6, y = 2352, label = "2404", fontface = "bold")
