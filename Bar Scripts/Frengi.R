76fcb0caab13f624d3e5a7f36d80eecdb180253b
samirnoff <54136421+samirnoff@users.noreply.github.com>
2019-11-28 15:50:58 +0300

    Update Frengi.R

--- Bar Scripts/Frengi.R
+++ Bar Scripts/Frengi.R
@@ -26,7 +26,7 @@ annolabel1 <- c("","17","17","14","32","32","56","10","24","36","25","31")
 annolabel2 <- c("108","25","33","69","84","62","3","5","13","29","38","70")
 annolabel3 <- c("93","115","72","61","80","41","154","172","173","201","177","202")
 annolabel4 <- c("","","","","","","","","26","34","35","28")
-cizgi <- c(0.02,0.06,0.1,0.14)
+cizgi <- c(0,0.06,0.12)
 ek <- seq(0,11)
 xline <- c(1925.65) + ek
 xlinend <- c(1926.35) + ek
@@ -49,7 +49,7 @@ ggplot(frengi, aes(x = zaman, y = deger, fill = grup, color = grup)) +
   annotate("segment", x = xline, xend = xlinend, y = 0, yend = 0, size = 1.1) +
   annotate("text", x = 1931, y = 230, label = "Devlet Frengi Mücadele Teşkilatı tarafından tedavi için", size = 5) +
   annotate("text", x = 1931, y = 218, label = "Vilayetlere gönderilen ilaçlar", size = 6, fontface = "bold") +
-  annotate("text", x = 1931, y = 208, label = "1926 \u2013 1937", size = 4.5) +
+  annotate("text", x = 1931, y = 205, label = "1926 \u2013 1937", size = 4.5) +
   annotate("text",x = xcor1, y = ycor1, label = annolabel1, angle = 90, fontface = "bold.italic",
            hjust = -0.2, size = 3) +
   annotate("text",x = xcor2, y = ycor2, label = annolabel2, angle = 90, fontface = "bold.italic",
@@ -77,9 +77,9 @@ ggplot(frengi, aes(x = zaman, y = deger, fill = grup, color = grup)) +
   annotate("rect", xmin = 1928 , xmax = 1928.2 , ymin = 183, ymax = 188, colour = "black", fill= "white", size = 1) +
   annotate("rect", xmin = 1928 , xmax = 1928.2 , ymin = 176, ymax = 181, colour = "black", fill= "white", size = 1) +
   annotate("rect", xmin = 1928 , xmax = 1928.2 , ymin = 169, ymax = 174, colour = "black", fill= "white", size = 1) +
-  annotate(geom="text", x=1928.8 ,y=192.5 ,label="Arsenobenzol", size= 3.2, fontface="italic") +
-  annotate(geom="text", x=1928.95 ,y=185.5 ,label="Bizmut müstahlebi", size= 3.2, fontface="italic") +
-  annotate(geom="text", x=1928.5 ,y=178.5 ,label="Civa", size= 3.2, fontface="italic") +
-  annotate(geom="text", x=1929.1 ,y=171.5 ,label="İyodür ve civa merhemi", size= 3.2, fontface="italic") +
-  annotate("segment", x = 1928.005 + cizgi, xend = 1928.005 + cizgi, y = 176, yend = 181, size = 1) +
+  annotate(geom="text", x=1929 ,y=192.5 ,label="Arsenobenzol", size= 3.2, fontface="italic") +
+  annotate(geom="text", x=1929.2 ,y=185.5 ,label="Bizmut müstahlebi", size= 3.2, fontface="italic") +
+  annotate(geom="text", x=1928.6 ,y=178.5 ,label="Civa", size= 3.2, fontface="italic") +
+  annotate(geom="text", x=1929.4 ,y=171.5 ,label="İyodür ve civa merhemi", size= 3.2, fontface="italic") +
+  annotate("segment", x = 1928.008 + cizgi, xend = 1928.008 + cizgi, y = 176, yend = 181, size = 1) +
   annotate("point", x=xlab, y=ylab, size=0.5, colour= "black")
